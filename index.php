abcc6f0a7e29efd8b9dd2d08446760258bdb9ceb
Olesya1311 <magalesenok@gmail.com>
2020-06-18 09:32:01 +0300

    завершена верстка

--- index.php
+++ index.php
@@ -48,7 +48,17 @@
               </div>
             </div>
             <div class="col-6"> <!--баланс, ATM и сдача-->
-              
+              <div class="input-group mb-3">
+                <input type="text" class="form-control balance" placeholder="Баланс">
+                <div class="input-group-append">
+                  <span class="input-group-text">&#8381;</span>
+                </div>
+              </div>
+              <div class="atm">
+                <img src="img/bill_acc.png" alt="">
+              </div>
+              <button class="btn btn-primary btn-block mt-3 change-btn">Сдача</button>
+              <div class="change-box mt-3"></div>
             </div>
           </div>
         </div><!--правая сторона (операционная)-->
